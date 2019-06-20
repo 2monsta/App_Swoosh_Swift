0b1c6c64022e6e9093359a234fe1e3575764247e
Jason Li <Yunf.li37@gmail.com>
2019-06-19 22:10:17 -0400

    added auto layout instead

--- App_Swoosh/App_Swoosh/ViewController.swift
+++ App_Swoosh/App_Swoosh/ViewController.swift
@@ -17,8 +17,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        swoosh.frame = CGRect(x: view.frame.size.width/2-swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
-        bgImg.frame = view.frame
+       // swoosh.frame = CGRect(x: view.frame.size.width/2-swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+        //bgImg.frame = view.frame
     }
 
 
