2967d632200a52f44576ad6e2d1cc79832b0eafd
Jason Li <Yunf.li37@gmail.com>
2019-06-21 00:35:18 -0400

    added manual segue

---
+++ App_Swoosh/App_Swoosh/Controller/SkillsVC.swift
@@ -0,0 +1,30 @@
+//
+//  SkillsVC.swift
+//  App_Swoosh
+//
+//  Created by yunfei on 6/20/19.
+//  Copyright © 2019 YTech. All rights reserved.
+//
+
+import UIKit
+
+class SkillsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
