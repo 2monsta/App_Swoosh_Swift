6361a60444bc070753b9682ef9c8a8e8a73824d4
Jason Li <Yunf.li37@gmail.com>
2019-06-22 14:48:28 -0400

    added button control where it would not go to finished page unless other button was selected

---
+++ App_Swoosh/App_Swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  App_Swoosh
+//
+//  Created by yunfei on 6/22/19.
+//  Copyright © 2019 YTech. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
